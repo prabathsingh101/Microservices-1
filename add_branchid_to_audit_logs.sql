17e0bc3a83836c4c6db62b140b6b6ec3bf5b95ec
Pappu Singh <Pappu Singh@LAPTOP-MM46D5U5>
2026-06-11 12:06:08 +0530

    pushed

--- add_branchid_to_audit_logs.sql
+++
@@ -1,9 +0,0 @@
-USE IdentityDb;
-GO
-
-IF NOT EXISTS (SELECT * FROM sys.columns WHERE object_id = OBJECT_ID(N'[dbo].[PermissionAuditLogs]') AND name = 'BranchId')
-BEGIN
-    ALTER TABLE [dbo].[PermissionAuditLogs] ADD [BranchId] NVARCHAR(100) NULL;
-    PRINT 'BranchId column added successfully.';
-END
-GO
